e4a6661a369a4b4b749d2c35f25cbef9fb41808a
gustavorodriDEV <gustavorodriguesoliveira012@gmail.com>
2024-07-20 17:21:56 -0300

    Payment, one-to-one association

--- src/main/java/com/estudandojava/course/config/TestConfig.java
+++ src/main/java/com/estudandojava/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.estudandojava.course.entities.Category;
 import com.estudandojava.course.entities.Order;
 import com.estudandojava.course.entities.OrderItem;
+import com.estudandojava.course.entities.Payment;
 import com.estudandojava.course.entities.Product;
 import com.estudandojava.course.entities.User;
 import com.estudandojava.course.entities.enums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 
