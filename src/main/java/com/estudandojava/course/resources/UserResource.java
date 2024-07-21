0c87d8f822f1cb2786f3327b28a120fb32bb612d
gustavorodriDEV <gustavorodriguesoliveira012@gmail.com>
2024-07-20 22:55:33 -0300

    User update

--- src/main/java/com/estudandojava/course/resources/UserResource.java
+++ src/main/java/com/estudandojava/course/resources/UserResource.java
@@ -16,6 +16,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.estudandojava.course.entities.User;
 import com.estudandojava.course.services.UserService;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RestController
 @RequestMapping(value = "/users")
@@ -48,5 +50,10 @@ public class UserResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-
+	
+	@PutMapping("path/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
 }
